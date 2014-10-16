0263ef11c0f654576bd05020a87017a906e1b771
Rob Nelson <guruvan@maza.club>
2014-10-16 16:25:20 +0000

    modify for docker builds directly fomr github, move out of ./build/

--- run-p2pool_maza.sh
+++ run-p2pool_maza.sh
@@ -1,4 +1,4 @@
 #use the repo set in $USER in Makefile
 #
-USER=`whoami`
-docker run -d -p 14477:14477 -p 14476:14476 -p 12835:12835 --name p2pool_maza ${USER}/p2pool-maza
+USER=ubuntu
+docker run -d -p 11177:14477 -p 11176:14476 -p 55835:12835 --name p2pool_maza ${USER}/p2pool-maza
